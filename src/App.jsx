e5ae1a6448923deef917bfe8312ef68a66191a33
aditya28n99 <adityawakale208@gmail.com>
2024-12-18 22:32:24 +0530

    react project setup with vite, tailwind and installed axios

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import {React} from "react";
+
+function App() {
+
+  return (
+    <div>
+      <h1 className="font-bold text-lg flex justify-center">Hello React!!</h1>
+    </div>
+  
+  );
+};
+
+export default App;
