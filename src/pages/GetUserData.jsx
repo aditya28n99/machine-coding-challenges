e523565c68e9cdb02e808a06d389d24b3e461477
aditya28n99 <adityawakale208@gmail.com>
2024-12-19 00:56:16 +0530

    added funtionalitys with toggeler

--- src/pages/GetUserData.jsx
+++ src/pages/GetUserData.jsx
@@ -3,6 +3,8 @@ import { fetchData } from "../services/getServices";
 
 export default function GetUserData() {
     const [data, setData] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [selectedId, setSelectedId] = useState(null);
 
     useEffect(() => {
 
@@ -10,6 +12,7 @@ export default function GetUserData() {
             try {
                 const result = await fetchData();
                 setData(result);
+                setIsLoading(false);
             }
             catch (e) {
                 console.log("Error from result:", e);
@@ -18,17 +21,34 @@ export default function GetUserData() {
         fetchUsers();
     }, []);
 
+    const showDrawer = (id) => {
+        setSelectedId((prevId) => (prevId === id ? null : id)); 
+    }
+
     return (
-        <div className="flex flex-col justify-center">
-            {
-                data.map((e, i) => {
-                    return <>
-                        <div key={e.id}>
-                            {e.title}
-                        </div>
-                    </>
-                })
+
+        <div className="flex flex-col p-5 m-5 gap-4 bg-gray-300">
+            <h1 className="justify-center flex">User Details</h1>
+            {isLoading ? <div>Loading...</div> :
+                <div className="flex flex-col gap-2">
+                    {
+                        data.map((e, i) => {
+                            return (
+                                <div key={e.id} className="flex flex-col gap-2 border rounded-md bg-blue-300 px-2">
+                                    <button onClick={() => showDrawer(e.id)}  className="cursor-pointer">
+                                        {e.title}
+                                    </button>
+                                    {selectedId === e.id && (
+                                        <div className="bg-white">{e.body}</div>
+                                    )}
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+
             }
+
         </div>
     )
 }
